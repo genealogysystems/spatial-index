9e07cac4376dc28757903328825084a02b68f5cd
John Clark <socrates37@gmail.com>
2013-12-26 12:36:21 -0700

    project setup

---
+++ src/main/java/com/findarecord/App.java
@@ -0,0 +1,10 @@
+package com.findarecord;
+
+public class App 
+{
+  // TODO use Apache Commons CLI Builder to get hostname and port
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
